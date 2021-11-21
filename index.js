39eaadec98d7307205d818eea0d04fa7501607a1
jiminsong490 <misosjm@naver.com>
2021-11-22 01:51:29 +0900

    fileComplete

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const bcrypt = require('bcrypt')
 const { application } = require('express')
 const saltRounds = 10
 
+const multer = require('multer')
+const e = require('express')
+const upload = multer({ dest: 'uploads/' })
+
 app.use(express.json())
 app.use(cors())
 
@@ -55,17 +59,49 @@ app.post('/exText', async (req, res) => {
     res.send({ success: result })
 })
 
-app.post('/fileUpload', async (req, res) => {
+app.post('/fileUpload', upload.single('testfile'), async (req, res) => {
     const connection = await mysql.createConnection({
         host: 'database-3.cjzvwuop4vpy.ap-northeast-2.rds.amazonaws.com',
         user: 'admin',
         password: 'rjHD2DB?WDHj6BDD$t&8EfJ8NTnbzGD9!=_Tp6Fdq',
         database: 'typing',
     })
-    // const form = new formidable.IncomingForm()
-    const asd = req.body.file
-    console.log(asd)
+    const asd = req.file
+    try {
+        const data = fs.readFileSync(asd.path, 'utf8')
+        console.log(data)
+        const db = await connection.execute(
+            'INSERT INTO `typing`.`exText` (`text`) VALUES (?)',
+            [data]
+        )
+        result = true
+        connection.destroy()
+    } catch (err) {
+        console.error(err)
+    }
 
-    res.send({ res: asd })
+    // const fileText = function (asd, callback) {
+    //     fs.readFile(asd.path, 'utf8', (err, data) => {
+    //         if (err) {
+    //             callback(err, null)
+    //             console.log(err)
+    //         } else {
+    //             callback(null, data.toString())
+    //             console.log(data.toString())
+    //         }
+    //     })
+    // }
+
+    // console.log(fileText)
+
+    // console.log('req : ', req)
+    // console.log('req.body : ', req.body)
+    // console.log('req.body.file : ', req.body.file)
+    // console.log('req : ', typeof req)
+    // console.log('req.body : ', typeof req.body)
+    // console.log('req.body.file : ', typeof req.body.file)
+    // connection.destroy()
+
+    res.send({ success: result })
 })
 app.listen(3712)
